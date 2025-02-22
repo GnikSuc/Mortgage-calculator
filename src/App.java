a133c1c300ba52b3e4a57ef7a07d13c4af0cdf01
GnikSuc <suchacek.n@gmail.com>
2025-02-22 11:40:13 +0100

    Improved error handling for invalid user input

--- src/App.java
+++ src/App.java
@@ -9,18 +9,50 @@ public class App {
         final byte PERCENT = 100;
 
         Scanner scanner = new Scanner(System.in);
+        String errorMessage;
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
-
-        System.out.print("Annual Interest Rate: ");
-        String input = scanner.next();
-        input = input.replace(",", ".");
-        float annualInterest = Float.parseFloat(input);
+        int principal;
+        errorMessage = "Enter a number between 1K and 1M (included).";
+        while (true) {
+            System.out.print("Principal (1K-1M): ");
+            try {
+                principal = Integer.parseInt(scanner.nextLine());
+                if (principal >= 1_000 && principal <= 1_000_000)
+                    break;
+                throw new IllegalArgumentException(errorMessage);
+            } catch (Exception e) {
+                System.out.println(errorMessage);
+            }
+        }
+        
+        float annualInterest;
+        errorMessage = "Enter a number greater than 0 and less than or equal to 30 (f.e. 3,99).";
+        while (true) { 
+            System.out.print("Annual Interest Rate: ");
+            try {
+                annualInterest = Float.parseFloat(scanner.nextLine().replace(",", "."));
+                if (annualInterest > 0 && annualInterest <= 30)
+                    break;
+                throw new IllegalArgumentException(errorMessage);
+            } catch (Exception e) {
+                System.out.println(errorMessage);
+            }
+        }
         float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
         
-        System.out.print("Period (Years): ");
-        byte years = scanner.nextByte();
+        byte years;
+        errorMessage = "Enter a value between 1 and 30 (included).";
+        while (true) { 
+            System.out.print("Period (Years): ");
+            try {
+                years = Byte.parseByte(scanner.nextLine());
+                if (years > 1 && years <= 30)
+                    break;
+                throw new IllegalArgumentException(errorMessage);
+            } catch (Exception e) {
+                System.out.println(errorMessage);
+            }
+        }
         int numberOfPayments = years * MONTHS_IN_YEAR;
 
         double mortgage = principal * ((monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))/(Math.pow(1 + monthlyInterest, numberOfPayments) - 1));
